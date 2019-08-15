75a4609a4da2f3a05afdef2f7a90503b523e92d7
OLBEA20 <olivier.beaulieu.7@ulaval.ca>
2019-08-15 19:33:59 -0400

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      {generateCellGrid(100, 100)}
+    </Container>
   );
 }
+
+const generateCellGrid = (width: number, height: number) => {
+  const rows: Array<JSX.Element> = new Array(height);
+  for(let i: number = 0; i < height; i++) {
+    rows.push(<Row key={i}>{generateCells(width)}</Row>);
+  }
+  return rows
+}
+
+const generateCells = (width: number) => {
+  const cells: Array<JSX.Element> = new Array(width)
+  for(let i: number = 0; i < width; i++) {
+    cells.push(<Cells key={i}></Cells>);
+  }
+  return cells;
+}
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 80vw;
+  height: 80vh;
+`;
+
+const Row = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  flex: 1;
+`;
+
+const Cells = styled.div`
+  border-style: solid;
+  border-width: 1px;
+  flex: 1;
+`;
 
 export default App;
